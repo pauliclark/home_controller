3fcf44d53de6f16f5d2987b74084b30e9ee76b8e
pauliclark <paul@venuescanner.com>
2021-02-12 12:19:04 +0000

    Clock screensaver

---
+++ src/clock.js
@@ -0,0 +1,83 @@
+// import $ from 'jquery'
+import './clock.scss'
+const updateMilliseconds = 20
+const clock = {
+  container: null,
+  display:false,
+  hands:{
+    hour:null,
+    minute:null,
+    second: null
+  },
+  updateInterval:null,
+  set: function() {
+    
+    const date = new Date()
+
+    const hours = ((date.getHours() + 11) % 12 + 1)
+    const minutes = date.getMinutes()
+    const seconds = date.getSeconds()
+    const ms = date.getMilliseconds()
+    
+    const minute = (minutes * 6) + (seconds / 6);
+    const hour = (hours * 30) + (minutes / 2);
+    const second = (updateMilliseconds === 1000) ? (seconds * 6) : ((seconds * 6) + (6*(ms/1000)))
+// console.log(updateMilliseconds, ms)
+    this.hands.hour.style.transform = `translate(-50%, -100%) rotate(${hour}deg)`
+    this.hands.minute.style.transform = `translate(-50%, -100%) rotate(${minute}deg)`
+    this.hands.second.style.transform = `translate(-50%, -100%) rotate(${second}deg)`
+  },
+  show:function(onHide) {
+    this.onHide = onHide
+    if (!this.container) {
+      const hours = []
+      for(let h = 1; h<=12; h++) {
+        hours.push(h)
+      }
+      this.container = document.createElement('div')
+      this.container.innerHTML = `<div class="dial">
+        <div class="wrap">
+    <span class="hour"></span>
+    <span class="minute"></span>
+    <span class="second"></span>
+    <span class="dot"></span>
+    ${hours.map(h => `<span class="tick" hour='${h}'></span>`).join("")}
+  </div>
+  </div>`
+      this.container.classList.add('clock')
+      this.container.addEventListener("click",() => {
+        this.hide()
+      })
+      document.body.append(this.container)
+      setTimeout(() => {
+        this.hands.hour = this.container.querySelector('.hour')
+        this.hands.minute = this.container.querySelector('.minute')
+        this.hands.second = this.container.querySelector('.second')
+        this.set()
+        clearInterval(this.updateInterval)
+        this.updateInterval = setInterval(() => {
+          this.set()
+        },updateMilliseconds)
+      })
+    }else{
+      
+        clearInterval(this.updateInterval)
+        this.updateInterval = setInterval(() => {
+          this.set()
+        },updateMilliseconds)
+    }
+      setTimeout(() => {
+        this.container.classList.add('show')
+        this.display = true
+      })
+  },
+  hide:function() {
+    if (this.container) {
+        this.container.classList.remove('show')
+        this.display = false
+        clearInterval(this.updateInterval)
+        this.onHide()
+    }
+  }
+}
+export default clock
--- src/index.js
+++ src/index.js
@@ -1,66 +1,85 @@
 import './main.scss'
-import $ from 'jquery'
+// import $ from 'jquery'
 import io from 'socket.io-client'
+import clock from './clock.js'
+let idleTimeout = null
+let idleDelay = 1000*60*3
+// idleDelay = 30000
+const showClock = () => {
+    clock.show(() => {
+        resetIdle()
+    })
+}
+const resetIdle = () => {
+    clearTimeout(idleTimeout)
+    idleTimeout = setTimeout(showClock, idleDelay)
+}
 const controller = {}
 const Light=function(button) {
     // console.log(button);
     this.on=false;
     this.ontoggle=function() {}
     this.toggle=function() {
+        resetIdle()
         // console.log(this.button.text())
-        this.ontoggle(this.button.text());
+        this.ontoggle(this.button.innerText);
     }
-    this.button=$(button).on("click",() => this.toggle())
+    this.button=button
+    button.addEventListener("click",() => this.toggle())
     Object.defineProperty(this, 'status', {
         get: function () {
             return this.on;
         },
         set: function (v) {
             this.on=v;
-            if (this.on) {
-                this.button.addClass("on");
-            }else{
-                this.button.removeClass("on");
-            }
+            // if (this.on) {
+                this.button.classList.toggle("on", this.on)
+            // }else{
+            //     this.button.removeClass("on");
+            // }
         }
     });
 }
 const Door=function(button) {
     this.open=function() {
-        this.button.addClass("on");
-        this.onopen(this.button.text());
+        resetIdle()
+                this.button.classList.add("on")
+        // this.button.addClass("on");
+        this.onopen(this.button.innerText)
         setTimeout(() => {
-            this.button.removeClass("on");
+                this.button.classList.remove("on")
         },3000)
     }
-    this.button=$(button).on("click",() => this.open())
+    this.button=button
+    this.button.addEventListener("click",() => this.open())
 }
 const SocketClient=function() {
     this.connected=function() {
         // console.log("Connected");
         this.socket.emit("status");
-        $('body').addClass("connected");
+        document.body.classList.add("connected")
+        resetIdle()
     }
     this.disconnected=function() {
-        $('body').removeClass("connected");
+        document.body.classList.remove("connected")
         // console.log("disconnected");
     }
     this.toggle=function(k) {
         // console.log("toggle",k);
         var obj={};
         obj[k]=true;
-        this.socket.emit('toggle',obj);
+        this.socket.emit('toggle',obj)
     }
     this.door=function(k) {
         // console.log("open",k);
         var obj={};
         obj[k]=true;
-        this.socket.emit('door',obj);
+        this.socket.emit('door',obj)
     }
     this.off=function() {
         // console.log("off");
         var obj={};
-        this.socket.emit('off',obj);
+        this.socket.emit('off',obj)
     }
     this.status=function(data) {
         for(var k in data) {
@@ -75,28 +94,29 @@ const SocketClient=function() {
         this.status(...args)
     })
 }
-$(() => {
+window.addEventListener('load',() => {
         // console.log(obj);
         controller.client = new SocketClient();
         controller.lights = {}
         controller.doors = {}
-        $('button.lights').get().map(b => {
+        document.querySelectorAll('button.lights').forEach(b => {
            var l = new Light(b);
            l.ontoggle=function(k) {
             //    console.log(k);
             controller.client.toggle(k);
            }
-            controller.lights[l.button.text()]=l;
+            controller.lights[l.button.innerText]=l;
         })
-        $('button.doors').get().map(b => {
+        document.querySelectorAll('button.doors').forEach(b => {
            var l = new Door(b);
            l.onopen=function(k) {
             //    console.log(k);
             controller.client.door(k);
            }
-            controller.doors[l.button.text()]=l;
+            controller.doors[l.button.innerText]=l;
         })
-        $('button.alloff').on("click",() => {
+        document.querySelector('button.alloff').addEventListener("click",() => {
             controller.client.off();
         })
+        resetIdle()
 })
